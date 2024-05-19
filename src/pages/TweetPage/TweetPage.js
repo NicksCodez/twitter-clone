2ab510cc509432dc241708a900baed5d0a0f0b1b
NicksCodez <elisei.nicolae08@gmail.com>
2024-05-19 15:20:20 +0300

    implement replies loading

--- src/pages/TweetPage/TweetPage.js
+++ src/pages/TweetPage/TweetPage.js
@@ -1,8 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 // firebase
-import { collection, getDocs, limit, query, where } from 'firebase/firestore';
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+  where,
+} from 'firebase/firestore';
 import { firestore } from '../../firebase';
 
 // css
@@ -10,10 +18,13 @@ import './TweetPage.css';
 
 // utils
 import svgs from '../../utils/svgs';
+import { attachListenersToTweets, tweetsLoader } from '../Home/Home';
 
 // components
 import PageHeader from '../../components/PageHeader/PageHeader';
 import FocusedTweet from '../../components/FocusedTweet/FocusedTweet';
+import ScrollableElementsLoader from '../../components/ScrollableElementsLoader/ScrollableElementsLoader';
+import Tweet from '../../components/Tweet/Tweet';
 
 const TweetPage = () => {
   // id parameter
@@ -24,6 +35,14 @@ const TweetPage = () => {
 
   // state to store tweet data
   const [currentTweetData, setCurrentTweetData] = useState(null);
+
+  // state to store loader info for ScrollableElementsLoader
+  const [loaderInfo, setLoaderInfo] = useState({});
+  const [replyTweets, setReplyTweets] = useState([]);
+  const seenLastReplyRef = useRef(false);
+  const lastVisibleReplyRef = useRef(null);
+  const [isScrollableLoading, seIsScrollableLoading] = useState(true);
+  const unsubscribersRef = useRef([]);
 
   // get post data
   useEffect(() => {
@@ -66,10 +85,59 @@ const TweetPage = () => {
     });
   }, [id]);
 
-  // debugging purposes
+  useEffect(
+    () => () => {
+      // unsubscribe from tweets listeners
+      // unsubsribersRef is an array, each element in it is an promise returned by tweetsLoader
+      // each of these promises resolves to an array returned by attachListeners...
+      // each element in this array is a function which unsubscribes from the tweets listener
+
+      unsubscribersRef.current.forEach((unsubbersArray) => {
+        unsubbersArray.forEach((unsubber) => {
+          unsubber();
+        });
+      });
+    },
+    []
+  );
+
   useEffect(() => {
-    console.log({ currentTweetData });
-  }, [currentTweetData]);
+    // once tweet data is loaded, create query for ScrollableElementsLoader
+    console.log('docRef changed => ', { currentTweetData });
+    if (currentTweetData?.docRef) {
+      const tweetsCollectionRef = collection(firestore, 'tweets');
+      const queryRef = query(
+        tweetsCollectionRef,
+        where('replyTo', '==', currentTweetData.docRef),
+        orderBy('createdAt', 'desc'),
+        limit(25)
+      );
+      setLoaderInfo({ queryRef });
+    }
+  }, [currentTweetData?.docRef]);
+
+  const handleIntersection = (entries) => {
+    const [entry] = entries;
+    // if already seen last tweet no need to run
+    if (entry.isIntersecting && !seenLastReplyRef.current) {
+      // construct query to get next 25 tweets after intersection
+      const tweetsCollectionRef = collection(firestore, 'tweets');
+
+      const queryRef = query(
+        tweetsCollectionRef,
+        where('replyTo', '==', currentTweetData.docRef),
+        orderBy('createdAt', 'desc'),
+        limit(25),
+        startAfter(lastVisibleReplyRef.current)
+      );
+
+      setLoaderInfo({ queryRef, attach: false });
+    }
+  };
+
+  const pushUnsubscriber = (unsubscriber) => {
+    unsubscribersRef.current.push(unsubscriber);
+  };
 
   // build left element
   const leftElement = (
@@ -108,6 +176,28 @@ const TweetPage = () => {
       {currentTweetData && (
         <FocusedTweet element={currentTweetData} ref={null} />
       )}
+      <div className="padded">
+        {loaderInfo.queryRef && (
+          <ScrollableElementsLoader
+            elementsLoader={loaderInfo}
+            elements={replyTweets}
+            setElements={setReplyTweets}
+            loadElements={tweetsLoader}
+            attachListenersToElements={attachListenersToTweets}
+            setLoadedSignal={seIsScrollableLoading}
+            intersectionHandler={handleIntersection}
+            ElementComponent={Tweet}
+            setLastRetrievedElement={(curRef) => {
+              lastVisibleReplyRef.current = curRef;
+            }}
+            seenLastElement={seenLastReplyRef}
+            setSeenLastElement={(curRef) => {
+              seenLastReplyRef.current = curRef;
+            }}
+            pushUnsubscriber={pushUnsubscriber}
+          />
+        )}
+      </div>
     </div>
   );
 };
